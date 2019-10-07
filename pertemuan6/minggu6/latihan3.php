4ab21f3a322eeeee1b0168f731bda908aaa01853
NurAnggraheni <nanggraheni88@gmail.com>
2019-10-07 16:53:00 +0700

    minggu-6

---
+++ pertemuan6/minggu6/latihan3.php
@@ -0,0 +1,9 @@
+<?php
+$nama=array("lanirne","aduh","qifuat","toda","anevi","samid","kifuat");
+    sort($nama);
+    for($i=0; $i<7;$i++) {
+        echo $nama[$i];
+        echo"<br>";
+    }
+   
+?>
