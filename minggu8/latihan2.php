4f6bedf3ee4265d33a52bbf5ee704793db781a09
NurAnggraheni <nanggraheni88@gmail.com>
2019-11-01 00:37:47 +0700

    Latihan 2 PHP

--- minggu8/latihan2.php
+++ minggu8/latihan2.php
@@ -1,17 +1,18 @@
- <HTML> 
+<HTML>
  <HEAD>
-        <title>Koneksi Database MySQL</title> 
-        </HEAD>
-        
-         <BODY> <h1>Koneksi database dengan mysql_fetch_assoc</h1>
-          <?php $conn=mysqli_connect ("localhost","root","") 
-           or die ("koneksi gagal");
-           mysqli_select_db($conn,"php"); 
-           $hasil = mysqli_query($conn,"select * from liga"); 
-           while ($row=mysqli_fetch_row($hasil)) {  
-                  echo "Liga " .$row[1]; 
-                   echo " mempunyai " .$row[2]; 
-                   echo " wakil di liga champion <br>"; } 
-                   ?>
-                    </BODY>
-     </HTML
+       <title>Koneksi Database MySQL</title>
+        </HEAD> 
+        <BODY> 
+               <h1>Koneksi database dengan mysql_fetch_array</h1> 
+        <?php $conn=mysqli_connect ("localhost","root","")  or die ("koneksi gagal");
+         mysqli_select_db($conn,"php");
+         $hasil = mysqli_query($conn,"select * from liga"); 
+         while ($row=mysqli_fetch_array($hasil))
+          {    
+               echo "Liga " .$row["Negara"]; //array asosiatif   
+               echo " mempunyai " .$row[2];  //array numeris    
+               echo " wakil di liga champion <br>"; 
+               } 
+        ?> 
+        </BODY> 
+</HTML>
